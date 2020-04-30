45e2589bcfc1d2a4e35256a0f0dcbb164742466f
Oxa <oxakost@gmail.com>
2020-05-01 01:22:47 +0300

    Updates

---
+++ precache-manifest.32fde13c4af1ef265ab41f9a69d9e292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8fc5a7809b5fdd4fe783ad75d562a8",
+    "url": "/hw-mainpage/index.html"
+  },
+  {
+    "revision": "dafe8914f70e0af3cb17",
+    "url": "/hw-mainpage/static/css/main.176e67d3.chunk.css"
+  },
+  {
+    "revision": "9d4fe9404e81ecd87718",
+    "url": "/hw-mainpage/static/js/2.5bd34e42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw-mainpage/static/js/2.5bd34e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafe8914f70e0af3cb17",
+    "url": "/hw-mainpage/static/js/main.f31412ec.chunk.js"
+  },
+  {
+    "revision": "254ea62cb70bc5f6f736",
+    "url": "/hw-mainpage/static/js/runtime-main.ee3a6416.js"
+  },
+  {
+    "revision": "adf3f16bee2eb2672ecd8cf3c1315595",
+    "url": "/hw-mainpage/static/media/avatar.adf3f16b.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/hw-mainpage/static/media/logo.ee7cd8ed.svg"
+  }
+]);
